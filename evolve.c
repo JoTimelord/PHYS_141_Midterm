29072e2fbe171ca1dad537a976c26548bf797f28
Yuntong Zhou <yuz102@its-may4623-09.ucsd.edu>
2022-03-09 14:34:49 -0800

    modified:   coremass.dat 	modified:   evolution.dat 	modified:   evolve 	modified:   evolve.c

--- evolve.c
+++ evolve.c
@@ -58,7 +58,7 @@ char *argv[];
     double tend;
     int particleN;
 
-    mstep = 500;            
+    mstep = 700;            
     nout = 1;                 
     scale = pow(10,8);
     dt = 0.1*scale;            
@@ -151,12 +151,12 @@ void coreaccel()
     double dist=0;
 
     for (j=0;j<kND;j++){
-        dist+=pow(r1[j],2);}
+        dist+=pow(r1[j]-r2[j],2);}
     dist=sqrt(dist);
     for (j=0;j<kND;j++)
     {
-       a1[j]=-G*M*(r1[j])/pow(dist,3);
-       a2[j]=-G*M*(r2[j])/pow(dist,3);
+       a1[j]=-G*M*(r1[j]-r2[j])/pow(dist,3);
+       a2[j]=-G*M*(r2[j]-r2[j])/pow(dist,3);
     }
     
 }
